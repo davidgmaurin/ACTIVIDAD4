e3cdba33431c19d03b8bd42c3797673a6dc58139
David González Maurín <davidmaurin@hotmail.com>
2023-04-01 13:44:03 +0200

    Versión 1

--- AE4202223/src/Calculadora.java
+++ AE4202223/src/Calculadora.java
@@ -1,4 +1,3 @@
-
 // Clase Calculadora
 // Fecha: 28/03/2023
 // Autor: David González Maurín
@@ -6,37 +5,59 @@
 // ---------------------------------------------------------------------------------------------------
 import java.util.Scanner;
 public class Calculadora {
-
-	public static void main(String[] args) {
+			//Método para sumar los números
+			public static int sumar(int numero1, int numero2) {
+				int suma = numero1 + numero2;
+				return suma;
+			}
 		
-		//Se crea un objeto de la clase Scanner para guardar los números
-		Scanner sc = new Scanner(System.in);
+			//Método para restar los números
+			public static int restar(int numero1, int numero2) {
+				int resta = numero1 - numero2;
+				return resta;
+			}
 			
-			//Se guarda el primer número
-			System.out.println("Introduzca el primer número:");
-			int numero1 = sc.nextInt();
+			//Método para multiplicar los números
+			public static int multiplicar(int numero1, int numero2) {
+				int multiplicacion = numero1 * numero2;
+				return multiplicacion;
+			}
+			
+			//Método para dividir los números
+			public static int dividir(int numero1, int numero2) {
+				int division = numero1 / numero2;
+				return division;
+			}
+			
+			//Método para visualizar los resultados
+			public static void resultados(int suma, int resta, int multiplicar, int division) {
+				System.out.println("La suma de los números es: " + suma);
+				System.out.println("La resta de los números es: " + resta);
+				System.out.println("La multiplicación de los números es: " + multiplicar);
+				System.out.println("La división de los números es: " + division);
+			}
+
+			public static void main(String[] args) {
+		
+				//Se crea un objeto de la clase Scanner para guardar los números
+				Scanner sc = new Scanner(System.in);
+			
+				//Se guarda el primer número
+				System.out.println("Introduzca el primer número:");
+				int numero1 = sc.nextInt();
 		    
-			//Se guarda el segundo número
-			System.out.println("Introduce el segundo número:");
-		    int numero2 = sc.nextInt();
+				//Se guarda el segundo número
+				System.out.println("Introduce el segundo número:");
+				int numero2 = sc.nextInt();
+				
+				int suma = sumar(numero1, numero2);
+		        int resta = restar(numero1, numero2);
+		        int multiplicacion = multiplicar(numero1, numero2);
+		        int division = dividir(numero1, numero2);
+		        
+		        resultados(suma, resta, multiplicacion, division);
 		    
-		    //Se suman ambos números
-		    int suma = numero1 + numero2;
-		    
-		    //Se restan ambos números
-		    int resta = numero1 - numero2;
-		    
-		    //Se multiplican ambos números
-		    int multiplicacion = numero1 * numero2;
-		    
-		    //Se dividen ambos números
-		    int division = numero1 / numero2;
-		    
-		    //Se muestra por pantalla los resultados de cada operación matemática
-		    System.out.println("La suma de los números es: " + suma);
-		    System.out.println("La resta de los números es: " + resta);
-		    System.out.println("La multiplicación de los números es: " + multiplicacion);
-		    System.out.println("La división de los números es: " + division);
-		  }
-		}
+			}
+		
+}
 
